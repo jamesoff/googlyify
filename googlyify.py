1909a305d0a863bb3cc76e8a406d32651dee343b
James Seward <sewardjm@amazon.com>
2018-08-30 21:07:27 +0100

    Tidy up python

--- googlyify.py
+++ googlyify.py
@@ -13,6 +13,56 @@ def get_landmark(which, landmarks, width, height):
     return (None, None)
 
 
+def upload_to_s3(filename, bucket, prefix):
+    s3 = boto3.client("s3")
+    print("Uploading to S3...")
+    with open(filename, "rb") as fh:
+        s3.put_object(
+            Body=fh, Bucket=bucket, Key="{}/{}".format(prefix, filename))
+
+
+def identify_faces(bucket, key):
+    print("Processing with rekognition...")
+    rekognition = boto3.client("rekognition")
+    result = rekognition.detect_faces(Image={
+        "S3Object": {
+            'Bucket': bucket,
+            'Name': key
+        }
+    })
+    return result
+
+
+def process_image(filename, face_details):
+    im = Image.open(filename)
+
+    width, height = im.size
+    draw = ImageDraw.Draw(im)
+    count = 0
+    for face in face_details:
+        count += 1
+        left_eye = get_landmark('eyeLeft', face['Landmarks'], width, height)
+        right_eye = get_landmark('eyeRight', face['Landmarks'], width, height)
+        dist = math.sqrt(((right_eye[0] - left_eye[0])**2) + (
+            (right_eye[1] - left_eye[1])**2)) / 2
+        print("l, r: ", left_eye, right_eye)
+        print("dist: ", dist)
+        print("brightness: ", face['Quality']['Brightness'])
+        print("sharpness: ", face['Quality']['Sharpness'])
+        print()
+        for eye in [left_eye, right_eye]:
+            (x, y) = eye
+            draw.ellipse([x - dist, y - dist, x + dist, y + dist],
+                         ImageColor.getrgb("white"))
+            draw.ellipse([x - (dist / 2), y, x + (dist / 2), y + dist],
+                         ImageColor.getrgb("black"))
+
+    if count:
+        print("Drew eyes on {} face(s)".format(count))
+
+    im.save("output.jpg")
+
+
 BUCKET = os.environ['GOOGLYIFY_BUCKET']
 PREFIX = os.environ['GOOGLYIFY_PREFIX']
 
@@ -23,20 +73,8 @@ except:
     json_filename = None
 
 if not json_filename:
-    s3 = boto3.client("s3")
-    print("Uploading to S3...")
-    with open(filename, "rb") as fh:
-        s3.put_object(
-            Body=fh, Bucket=BUCKET, Key="{}/{}".format(PREFIX, filename))
-
-    print("Processing with rekognition...")
-    rekognition = boto3.client("rekognition")
-    result = rekognition.detect_faces(Image={
-        "S3Object": {
-            'Bucket': BUCKET,
-            'Name': '{}/{}'.format(PREFIX, filename)
-        }
-    })
+    upload_to_s3(filename, BUCKET, PREFIX)
+    result = identify_faces(BUCKET, '{}/{}'.format(PREFIX, filename))
     fh = open('output.json', 'w')
     json.dump(result, fh)
     fh.close()
@@ -45,30 +83,4 @@ else:
     result = json.load(fh)
     fh.close()
 
-im = Image.open(filename)
-
-width, height = im.size
-draw = ImageDraw.Draw(im)
-count = 0
-for face in result['FaceDetails']:
-    count += 1
-    left_eye = get_landmark('eyeLeft', face['Landmarks'], width, height)
-    right_eye = get_landmark('eyeRight', face['Landmarks'], width, height)
-    dist = math.sqrt(((right_eye[0] - left_eye[0])**2) + (
-        (right_eye[1] - left_eye[1])**2)) / 2
-    print("l, r: ", left_eye, right_eye)
-    print("dist: ", dist)
-    print("brightness: ", face['Quality']['Brightness'])
-    print("sharpness: ", face['Quality']['Sharpness'])
-    print()
-    for l in ['eyeLeft', 'eyeRight']:
-        (x, y) = get_landmark(l, face['Landmarks'], width, height)
-        draw.ellipse([x - dist, y - dist, x + dist, y + dist],
-                     ImageColor.getrgb("white"))
-        draw.ellipse([x - (dist / 2), y, x + (dist / 2), y + dist],
-                     ImageColor.getrgb("black"))
-
-if count:
-    print("Drew eyes on {} face(s)".format(count))
-
-im.save("output.jpg")
+process_image(filename, result['FaceDetails'])
