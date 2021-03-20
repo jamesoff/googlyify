4a7a31052475c364491c6ae47f874daf9a6e3c3f
James Seward <james@jamesoff.net>
2021-03-20 13:43:49 +0000

    Apply black formatting

--- googlyify.py
+++ googlyify.py
@@ -8,8 +8,8 @@ from PIL import Image, ImageDraw, ImageColor
 
 def get_landmark(which, landmarks, width, height):
     for landmark in landmarks:
-        if landmark['Type'] == which:
-            return (landmark['X'] * width, landmark['Y'] * height)
+        if landmark["Type"] == which:
+            return (landmark["X"] * width, landmark["Y"] * height)
     return (None, None)
 
 
@@ -17,19 +17,15 @@ def upload_to_s3(filename, bucket, prefix):
     s3 = boto3.client("s3")
     print("Uploading to S3...")
     with open(filename, "rb") as fh:
-        s3.put_object(
-            Body=fh, Bucket=bucket, Key="{}/{}".format(prefix, filename))
+        s3.put_object(Body=fh, Bucket=bucket, Key="{}/{}".format(prefix, filename))
 
 
 def identify_faces(bucket, key):
     print("Processing with rekognition...")
     rekognition = boto3.client("rekognition")
-    result = rekognition.detect_faces(Image={
-        "S3Object": {
-            'Bucket': bucket,
-            'Name': key
-        }
-    })
+    result = rekognition.detect_faces(
+        Image={"S3Object": {"Bucket": bucket, "Name": key}}
+    )
     return result
 
 
@@ -41,21 +37,29 @@ def process_image(filename, face_details):
     count = 0
     for face in face_details:
         count += 1
-        left_eye = get_landmark('eyeLeft', face['Landmarks'], width, height)
-        right_eye = get_landmark('eyeRight', face['Landmarks'], width, height)
-        dist = math.sqrt(((right_eye[0] - left_eye[0])**2) + (
-            (right_eye[1] - left_eye[1])**2)) / 2
+        left_eye = get_landmark("eyeLeft", face["Landmarks"], width, height)
+        right_eye = get_landmark("eyeRight", face["Landmarks"], width, height)
+        dist = (
+            math.sqrt(
+                ((right_eye[0] - left_eye[0]) ** 2)
+                + ((right_eye[1] - left_eye[1]) ** 2)
+            )
+            / 2
+        )
         print("l, r: ", left_eye, right_eye)
         print("dist: ", dist)
-        print("brightness: ", face['Quality']['Brightness'])
-        print("sharpness: ", face['Quality']['Sharpness'])
+        print("brightness: ", face["Quality"]["Brightness"])
+        print("sharpness: ", face["Quality"]["Sharpness"])
         print()
         for eye in [left_eye, right_eye]:
             (x, y) = eye
-            draw.ellipse([x - dist, y - dist, x + dist, y + dist],
-                         ImageColor.getrgb("white"))
-            draw.ellipse([x - (dist / 2), y, x + (dist / 2), y + dist],
-                         ImageColor.getrgb("black"))
+            draw.ellipse(
+                [x - dist, y - dist, x + dist, y + dist], ImageColor.getrgb("white")
+            )
+            draw.ellipse(
+                [x - (dist / 2), y, x + (dist / 2), y + dist],
+                ImageColor.getrgb("black"),
+            )
 
     if count:
         print("Drew eyes on {} face(s)".format(count))
@@ -63,8 +67,8 @@ def process_image(filename, face_details):
     im.save("output.jpg")
 
 
-BUCKET = os.environ['GOOGLYIFY_BUCKET']
-PREFIX = os.environ['GOOGLYIFY_PREFIX']
+BUCKET = os.environ["GOOGLYIFY_BUCKET"]
+PREFIX = os.environ["GOOGLYIFY_PREFIX"]
 
 filename = sys.argv[1]
 try:
@@ -74,13 +78,13 @@ except:
 
 if not json_filename:
     upload_to_s3(filename, BUCKET, PREFIX)
-    result = identify_faces(BUCKET, '{}/{}'.format(PREFIX, filename))
-    fh = open('output.json', 'w')
+    result = identify_faces(BUCKET, "{}/{}".format(PREFIX, filename))
+    fh = open("output.json", "w")
     json.dump(result, fh)
     fh.close()
 else:
-    fh = open(json_filename, 'r')
+    fh = open(json_filename, "r")
     result = json.load(fh)
     fh.close()
 
-process_image(filename, result['FaceDetails'])
+process_image(filename, result["FaceDetails"])
